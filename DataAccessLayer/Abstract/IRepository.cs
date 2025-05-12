3dae4c2a42470db3cd60d741a25068ff717add23
Berkay Yıldız <yildizberkay359@gmail.com>
2025-05-12 15:33:28 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        void Insert(T p);
+
+        // Sildirceğimiz değeri buldurmamız gerekiyor
+        T Get(Expression<Func<T, bool>> filter);
+        void Delete(T p);
+        void Update(T p);
+
+        // Şartlı listeleme için kullanacağım metot
+        // Mesela yazar adı ali olanları getir dersek bu metot sayesinde getiricez.
+
+        List<T> List(Expression<Func<T, bool>> filter);
+
+    }
+}
